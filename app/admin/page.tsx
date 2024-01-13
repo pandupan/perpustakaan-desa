5cb5bfe850cc208a1ed1808a56089d6744b2c1ab
pandupan <pangestup60@gmail.com>
2024-01-13 11:50:42 +0700

    [ADD] - Admin Page

---
+++ app/admin/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "TailAdmin | Next.js E-commerce Dashboard Template",
+  description: "This is Home Blog page for TailAdmin Next.js",
+};
+
+export default function Home() {
+  return (
+    <>
+      <div>Hallo</div>
+    </>
+  );
+}
