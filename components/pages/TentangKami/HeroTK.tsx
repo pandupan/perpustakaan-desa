ca8dcc49d909c48233da02f1b5b96ece587c7083
EgaAprianto <egaaprianto04@gmail.com>
2024-01-20 20:09:52 +0700

    [ADD] IMAGE HERO & TENTANG KAMI

--- components/pages/TentangKami/HeroTK.tsx
+++ components/pages/TentangKami/HeroTK.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { IoNotificationsOutline, IoSearchOutline } from "react-icons/io5";
 import { FaRegBookmark } from "react-icons/fa";
+import Image from "next/image";
 
 const HeroTK = () => {
+  const ImageURL = [
+    {
+      image: "https://i.ibb.co/BcVJGqT/slidehero1.png"
+    }
+  ];
+  const backgroundImageStyle = {
+    backgroundImage: `url(${ImageURL[0].image})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  };
+
   return (
-    <div className="px-10 h-[600px] bg-white">
-      <div className="container px-10 flex flex-col gap-14 justify-center h-full">
-        <div className="flex flex-col w-[500px] text-[#1a1668] ">
-          <h1 className="text-5xl font-bold">Perpustakaan Desa</h1>
-          <h2 className="text-2xl font-semibold">Website Perpustakaan</h2>
+    <div className="relative px-10 h-[400px] bg-white" style={backgroundImageStyle}>
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="container">
+          <h1 className="text-5xl font-bold text-white">Perpustakaan Desa</h1>
+          <h2 className="text-2xl font-semibold text-white">Website Perpustakaan</h2>
         </div>
       </div>
     </div>
